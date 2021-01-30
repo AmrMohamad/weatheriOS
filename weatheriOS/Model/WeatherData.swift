e5db92bea3479396c8e33e7f5e9343e65e2f0281
AmrMohamad <amr.mohamed124124@gmail.com>
2021-01-30 21:20:04 +0200

    fetch data from api using JSONdecoder

---
+++ weatheriOS/Model/WeatherData.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherData.swift
+//  weatheriOS
+//
+//  Created by Amr Mohamad on 1/30/21.
+//  Copyright © 2021 Amr Mohamad. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name    : String
+    let main    : Main
+    let weather : [Weather]
+}
+
+struct Main: Decodable {
+    let temp : Double
+}
+
+struct Weather: Decodable {
+    let description :String
+}
--- weatheriOS/Model/WeatherManager.swift
+++ weatheriOS/Model/WeatherManager.swift
@@ -12,7 +12,36 @@ struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?units=metric"
     
     func fetchWeather(cityName: String)  {
-        let url = "\(weatherURL)appid=\(Constants.apiKey)&q=\(cityName)"
+        let url = "\(weatherURL)&appid=\(Constants.apiKey)&q=\(cityName)"
+        performRequest(urlString: url)
         print(url)
+    }
+    
+    func performRequest(urlString: String) {
+        if let urlFetched = URL(string: urlString){
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: urlFetched) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    self.parseJSON(weatherData: safeData)
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do{
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.weather[0].description)
+        }catch{
+            print(error)
+        }
+        
     }
 }
